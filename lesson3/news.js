acc16a1481914fec3f4003958fd615c58fafd9d8
Marina Golovkina <marina.golovkina@mail.ru>
2016-02-06 16:43:18 +0700

    - Get news by request and render them to the browser.

--- lesson3/news.js
+++ lesson3/news.js
@@ -1,18 +1,29 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var http = require('http');
 
-request('https://habrahabr.ru/', function(err, response, html){
-	if (!err && response.statusCode == 200)
-	{
-		var $ = cheerio.load(html);
-		$('div.shortcuts_item').each(function(i, elem){
-			$(this).find('.content .buttons').remove();
-			$(this).find('.content img').remove();
+http.createServer(function(req, res){
+	res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
 
-			console.log('Дата: ' + $(this).find('.published').text());
-			console.log('Заголовок: ' + $(this).find('.title a').text());
-			console.log('Источник: ' + $(this).find('.title a').attr('href'));
-			console.log('Содержание: ' + $(this).find('.content').text());
-		});
-	}
-});
+	request('https://habrahabr.ru/', function(err, response, html){
+		if (!err && response.statusCode == 200)
+		{
+			var $ = cheerio.load(html);
+			$('div.shortcuts_item').each(function(i, elem){
+				$(this).find('.content .buttons').remove();
+				$(this).find('.content img').remove();
+
+				res.write('<div style="margin: 40px">');
+				res.write('<h2><a href="' + $(this).find('.title a').attr('href') + '">' 
+							+ $(this).find('.title a').text() + '</a></h2>');
+				res.write('<p><em>Дата: ' + $(this).find('.published').text() + '</em></p>');
+				res.write('<p>' + $(this).find('.content') + '</p>');
+				res.write('</div>');
+			});
+			res.end();
+		}
+	});
+
+}).listen(5000);
+
+console.log('Server has started!');
